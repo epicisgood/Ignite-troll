7d0813eaae0e7eb632b8938f0640db4e6f8ca521
epicisgood <144903320+epicisgood@users.noreply.github.com>
2023-11-20 18:56:07 +0000

    lets go

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import discord.utils
 from cogs import Simple_commands, Troll_commands
 from school import Link_commands
 
-
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=".", intents=intents)
 my_secret = os.environ['TOKEN']
+
 
 @client.event
 async def on_ready():
@@ -26,22 +26,16 @@ async def on_ready():
 async def load_cogs():
   await client.wait_until_ready()
   try:
-      simple_command_cog = Simple_commands(client)
-      troll_commands_cog = Troll_commands(client)
-      link_command_cog = Link_commands(client)
-      await client.add_cog(simple_command_cog)
-      await client.add_cog(troll_commands_cog)
-      await client.add_cog(link_command_cog)
+    simple_command_cog = Simple_commands(client)
+    troll_commands_cog = Troll_commands(client)
+    link_command_cog = Link_commands(client)
+    await client.add_cog(simple_command_cog)
+    await client.add_cog(troll_commands_cog)
+    await client.add_cog(link_command_cog)
   except Exception as e:
-      print(f"Error loading cogs: {e}")
-
-
+    print(f"Error loading cogs: {e}")
 
 
 #asfkjaslkfjskdafjljljsdakf
-
-
-
-
 
 client.run(my_secret)
