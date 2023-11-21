a1a140a2a1ecb1763e44e380baf376bc48c3621e
epicisgood <144903320+epicisgood@users.noreply.github.com>
2023-11-21 12:40:32 +0000

    Changed regular commands to bridge commands

--- main.py
+++ main.py
@@ -1,41 +1,21 @@
 import os
 
 import discord
-from discord.ext import commands
-import discord.utils
-
-from cogs import Simple_commands, Troll_commands
-from school import Link_commands
+from discord.ext import bridge
 
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix=".", intents=intents)
-my_secret = os.environ['TOKEN']
+client = bridge.Bot(command_prefix="!", intents=intents)
 
+
+my_secret = os.environ['TOKEN']
 
 @client.event
 async def on_ready():
   print(f"{client.user} is working")
-  await load_cogs()
-  try:
-    synced = await client.tree.sync()
-    print(f"Synced {len(synced)} commands")
-  except Exception as e:
-    print(e)
+  
 
 
-async def load_cogs():
-  await client.wait_until_ready()
-  try:
-    simple_command_cog = Simple_commands(client)
-    troll_commands_cog = Troll_commands(client)
-    link_command_cog = Link_commands(client)
-    await client.add_cog(simple_command_cog)
-    await client.add_cog(troll_commands_cog)
-    await client.add_cog(link_command_cog)
-  except Exception as e:
-    print(f"Error loading cogs: {e}")
 
-
-#asfkjaslkfjskdafjljljsdakf
+client.load_extensions("cogs","school")
 
 client.run(my_secret)
--- school.py
+++ school.py
@@ -1,6 +1,5 @@
 import discord
-from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, bridge
 import discord.utils
 
 
@@ -8,22 +7,15 @@ import discord.utils
 class Link_commands(commands.Cog):
   def __init__(self, client):
       self.client = client
-  @app_commands.command(name="link", description="Link button ")
-  async def link(self, interaction: discord.Interaction):
-    await interaction.response.send_message(embed=discord.Embed(title="Links!", description="Click the link button below!", color=0x00ff00), view=Interstellar_buttons())
-
-
-
-
-
+  @bridge.bridge_command(aliases=["links"],description="Link dispenser for interstellar links")
+  async def link(self, ctx):
+    await ctx.reply(embed=discord.Embed(title="Links!", description="Click the link button below!", color=0x00ff00), view=Interstellar_buttons())
 
 
 
 class Interstellar_buttons(discord.ui.View):
-  def __init__(self):
-    super().__init__(timeout=None)
   @discord.ui.button(label="Private links", style=discord.ButtonStyle.primary)
-  async def link(self,interaction: discord.Interaction, button: discord.ui.Button):
+  async def link(self,button, interaction: discord.Interaction):
     if interaction.user.id == 726162926851063919: #Myself lol
       await interaction.response.send_message(content="lol this is so cool guys",ephemeral=True)
     elif interaction.user.id == 1154785751293247548:#jackcampbell
@@ -33,11 +25,11 @@ class Interstellar_buttons(discord.ui.View):
     elif interaction.user.id == 1020747577240588289: #Bando Jr
       await interaction.response.send_message(content=" https://common-language.onrender.com/", ephemeral=True) 
     else:
-      await interaction.response.send_message(content="@_epic. me for your personal link and ill add it to the link bot", ephemeral=True)
+      await interaction.response.send_message(content=f"{self.client.owner.mention} me for your personal link and ill add it to the link bot", ephemeral=True)
 
 
   @discord.ui.button(label="Public links", style=discord.ButtonStyle.secondary)
-  async def public_links(self, interaction: discord.Interaction, button: discord.ui.Button):
+  async def public_links(self,button, interaction: discord.Interaction, ):
     links_public = {
       '1st link': "https://royal-king.onrender.com/",
       '2nd link':"https://checkers-jacket.onrender.com/",
@@ -48,3 +40,8 @@ class Interstellar_buttons(discord.ui.View):
     for key, value in links_public.items():
       public_link_embed.add_field(name=key, value=value, inline=False)
     await interaction.response.send_message(content= "**You can share these link to anyone**", embed=public_link_embed, ephemeral=True)
+    
+
+
+def setup(client):
+  client.add_cog(Link_commands(client))
